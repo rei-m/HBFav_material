27a10758618530d3d86b67642b232e4abd907c4e
rei-m <star.oceans.rm@wj8.so-net.ne.jp>
2015-12-23 02:08:53 +0900

    不要なimportを削除

--- app/src/main/java/me/rei_m/hbfavmaterial/activities/BaseActivityWithDrawer.kt
+++ app/src/main/java/me/rei_m/hbfavmaterial/activities/BaseActivityWithDrawer.kt
@@ -10,7 +10,6 @@ import android.support.v7.widget.AppCompatImageView
 import android.support.v7.widget.AppCompatTextView
 import android.support.v7.widget.Toolbar
 import android.view.MenuItem
-import android.view.View
 import com.squareup.picasso.Picasso
 import me.rei_m.hbfavmaterial.R
 import me.rei_m.hbfavmaterial.events.EventBusHolder
