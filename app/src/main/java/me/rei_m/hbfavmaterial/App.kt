62eb8f1315b71594e124ef6db1074aeec674926d
rei-m <star.oceans.rm@wj8.so-net.ne.jp>
2015-12-26 00:16:15 +0900

    Debugの時はCrashlitcsを使用しない

--- app/src/main/java/me/rei_m/hbfavmaterial/App.kt
+++ app/src/main/java/me/rei_m/hbfavmaterial/App.kt
@@ -2,7 +2,6 @@ package me.rei_m.hbfavmaterial
 
 import android.app.Application
 import com.crashlytics.android.Crashlytics
-import com.crashlytics.android.core.CrashlyticsCore
 import io.fabric.sdk.android.Fabric
 import me.rei_m.hbfavmaterial.managers.ModelLocator
 import me.rei_m.hbfavmaterial.managers.ModelLocator.Companion.Tag
@@ -15,13 +14,10 @@ public class App : Application() {
 
         // Application起動時に実行される。アプリの初期処理など
 
-        // Set up Crashlytics, disabled for debug builds
-        val crashlyticsKit = Crashlytics.Builder()
-                .core(CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build())
-                .build()
-
-        // Initialize Fabric with the debug-disabled crashlytics.
-        Fabric.with(this, crashlyticsKit);
+        // Set up Crashlytics
+        if (!BuildConfig.DEBUG) {
+            Fabric.with(this, Crashlytics());
+        }
 
         // ModelLocatorにModelの参照を登録
         ModelLocator.apply {
