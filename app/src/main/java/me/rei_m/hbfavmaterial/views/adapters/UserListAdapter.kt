03fa06e54c29c238a0aed17c05fb255db9df4bb6
rei-m <star.oceans.rm@wj8.so-net.ne.jp>
2016-01-24 10:29:21 +0900

    Data Bindingの導入

--- app/src/main/java/me/rei_m/hbfavmaterial/views/adapters/UserListAdapter.kt
+++ app/src/main/java/me/rei_m/hbfavmaterial/views/adapters/UserListAdapter.kt
@@ -1,12 +1,17 @@
 package me.rei_m.hbfavmaterial.views.adapters
 
 import android.content.Context
+import android.databinding.DataBindingUtil
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.squareup.picasso.Picasso
 import me.rei_m.hbfavmaterial.R
+import me.rei_m.hbfavmaterial.databinding.ListItemUserBinding
 import me.rei_m.hbfavmaterial.entities.BookmarkEntity
-import me.rei_m.hbfavmaterial.views.widgets.list.UserItemLayout
+import me.rei_m.hbfavmaterial.utils.BookmarkUtil
+import me.rei_m.hbfavmaterial.views.widgets.graphics.RoundedTransformation
 
 /**
  * ユーザー一覧を管理するAdaptor.
@@ -14,12 +19,33 @@ import me.rei_m.hbfavmaterial.views.widgets.list.UserItemLayout
 class UserListAdapter constructor(context: Context, resource: Int) :
         ArrayAdapter<BookmarkEntity>(context, resource) {
 
+    val mLayoutInflater = LayoutInflater.from(context)
+
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View? {
 
-        val view = convertView ?: View.inflate(context, R.layout.list_item_user, null)
+        val bookmarkEntity = getItem(position)
 
-        (view as UserItemLayout).bindView(getItem(position))
+        val view = convertView?.apply {
+            val binding = DataBindingUtil.getBinding<ListItemUserBinding>(this)
+            setBinding(binding, bookmarkEntity)
+        } ?: let {
+            val binding = DataBindingUtil.inflate<ListItemUserBinding>(mLayoutInflater, R.layout.list_item_user, parent, false)
+            setBinding(binding, bookmarkEntity)
+            return binding.root
+        }
 
         return view
+    }
+
+    private fun setBinding(binding: ListItemUserBinding, bookmarkEntity: BookmarkEntity) {
+        binding.bookmarkEntity = bookmarkEntity
+
+        // TODO XMLの中で書きたいところだけど、XML側でimportしてもstaticメソッドがいないと言われるので、いったんこっちに
+        binding.listItemUserTextAddBookmarkTiming.text = BookmarkUtil.getPastTimeString(bookmarkEntity.date)
+
+        Picasso.with(context)
+                .load(BookmarkUtil.getIconImageUrlFromId(bookmarkEntity.creator))
+                .transform(RoundedTransformation())
+                .into(binding.listItemUserImageIcon)
     }
 }
