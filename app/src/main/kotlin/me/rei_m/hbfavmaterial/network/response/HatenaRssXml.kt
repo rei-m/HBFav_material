58ce26ffc0a5e8f0756ef11ff367592420b1d426
rei-m <star.oceans.rm@wj8.so-net.ne.jp>
2016-05-13 08:23:37 +0900

    add xml parse

--- app/src/main/kotlin/me/rei_m/hbfavmaterial/network/response/HatenaRssXml.kt
+++ app/src/main/kotlin/me/rei_m/hbfavmaterial/network/response/HatenaRssXml.kt
@@ -1,17 +1,11 @@
 package me.rei_m.hbfavmaterial.network.response
 
-import org.simpleframework.xml.Element
 import org.simpleframework.xml.ElementList
 import org.simpleframework.xml.Root
 
 @Root(name = "rdf:RDF", strict = false)
 class HatenaRssXml {
-
-    @set:ElementList(name = "item", inline = true, required = true)
-    private var list: List<HatenaRssItemXml>? = null
-
-    @set:Element(name = "item", required = true)
-    private var item: HatenaRssItemXml? = null
-
-    fun getList(): List<HatenaRssItemXml> = list!!
+    @set:ElementList(inline = true)
+    @get:ElementList(inline = true)
+    var list: List<HatenaRssItemXml>? = null
 }
--- app/src/main/kotlin/me/rei_m/hbfavmaterial/repositories/BookmarkRepository.kt
+++ app/src/main/kotlin/me/rei_m/hbfavmaterial/repositories/BookmarkRepository.kt
@@ -4,10 +4,7 @@ import kotlinx.dom.parseXml
 import me.rei_m.hbfavmaterial.entities.ArticleEntity
 import me.rei_m.hbfavmaterial.entities.BookmarkEntity
 import me.rei_m.hbfavmaterial.enums.ReadAfterFilter
-import me.rei_m.hbfavmaterial.network.BookmarkOwnRss
-import me.rei_m.hbfavmaterial.network.EntryApi
-import me.rei_m.hbfavmaterial.network.HatenaRssService
-import me.rei_m.hbfavmaterial.network.HttpClient
+import me.rei_m.hbfavmaterial.network.*
 import me.rei_m.hbfavmaterial.utils.ApiUtil
 import me.rei_m.hbfavmaterial.utils.RssXmlUtil
 import retrofit2.Retrofit
@@ -25,9 +22,9 @@ open class BookmarkRepository() {
      * お気に入りのユーザーのブックマーク情報を取得する.
      */
     open fun findByUserIdForFavorite(userId: String, startIndex: Int = 0): Observable<List<BookmarkEntity>> {
-        //        return BookmarkFavoriteRss()
-        //                .request(userId, startIndex)
-        //                .map { response -> parseRssResponse(response) }
+//                return BookmarkFavoriteRss()
+//                        .request(userId, startIndex)
+//                        .map { response -> parseRssResponse(response) }
         val adapter = Retrofit.Builder()
                 .baseUrl("http://b.hatena.ne.jp")
                 .client(HttpClient.instance)
@@ -37,7 +34,13 @@ open class BookmarkRepository() {
         return adapter.create(HatenaRssService::class.java)
                 .favorite(userId, startIndex)
                 .map { response ->
-                    println(response.getList().size)
+                    response.list ?: return@map arrayListOf<BookmarkEntity>()
+                    println(response.list!![0].title)
+                    println(response.list!![0].creator)
+                    println(response.list!![0].dateString)
+                    println(response.list!![0].bookmarkCount)
+                    println(response.list!![0].content)
+
                     arrayListOf<BookmarkEntity>()
                 }
     }
