62d087e4f3d058c5053679be2f582aa635661792
rei-m <star.oceans.rm@wj8.so-net.ne.jp>
2016-05-17 07:52:39 +0900

    add logger for retrofit

--- app/src/main/kotlin/me/rei_m/hbfavmaterial/network/HatenaApiService.kt
+++ app/src/main/kotlin/me/rei_m/hbfavmaterial/network/HatenaApiService.kt
@@ -2,10 +2,10 @@ package me.rei_m.hbfavmaterial.network
 
 import me.rei_m.hbfavmaterial.network.response.BookmarkEntryResponse
 import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.Query
 import rx.Observable
 
 interface HatenaApiService {
-    @GET("entry/jsonlite/{entryUrl}")
-    fun entry(@Path("entryUrl") entryUrl: String): Observable<BookmarkEntryResponse>
+    @GET("entry/jsonlite/")
+    fun entry(@Query("url") entryUrl: String): Observable<BookmarkEntryResponse>
 }
--- app/src/main/kotlin/me/rei_m/hbfavmaterial/network/response/BookmarkEntryResponse.kt
+++ app/src/main/kotlin/me/rei_m/hbfavmaterial/network/response/BookmarkEntryResponse.kt
@@ -1,22 +1,19 @@
 package me.rei_m.hbfavmaterial.network.response
 
 class BookmarkEntryResponse {
-
-    companion object {
-        class Entity() {
-            @JvmField var timestamp: String = ""
-
-            @JvmField var comment: String = ""
-
-            @JvmField var user: String = ""
-
-            @JvmField var tags: List<String> = arrayListOf()
-        }
-    }
-
     var count: Int = 0
         get
         set
 
     var bookmarks: List<Entity> = arrayListOf()
+}
+
+class Entity() {
+    var timestamp: String = ""
+
+    var comment: String = ""
+
+    var user: String = ""
+
+    var tags: List<String> = arrayListOf()
 }
