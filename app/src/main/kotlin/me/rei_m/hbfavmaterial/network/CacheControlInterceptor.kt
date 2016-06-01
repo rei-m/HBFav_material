ebccd29a45bcf8740cc7d6d1f5c4681d5b231139
rei-m <star.oceans.rm@wj8.so-net.ne.jp>
2016-06-02 08:23:49 +0900

    refactor netwark lib

---
+++ app/src/main/kotlin/me/rei_m/hbfavmaterial/network/CacheControlInterceptor.kt
@@ -0,0 +1,17 @@
+package me.rei_m.hbfavmaterial.network
+
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class CacheControlInterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val original = chain.request()
+        val requestBuilder = original.newBuilder()
+                .addHeader("Cache-Control", "no-cache")
+                .addHeader("Cache-Control", "no-store")
+                .method(original.method(), original.body())
+        val request = requestBuilder.build()
+
+        return chain.proceed(request)
+    }
+}
--- app/src/main/kotlin/me/rei_m/hbfavmaterial/network/HttpClient.kt
+++ app/src/main/kotlin/me/rei_m/hbfavmaterial/network/HttpClient.kt
@@ -1,30 +1,7 @@
 package me.rei_m.hbfavmaterial.network
 
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 
 object HttpClient {
-
-    val instance: OkHttpClient
-
-    init {
-
-        // TODO ビルドタイプかフレーバーでロガーのレベルを変える.
-        val logging = HttpLoggingInterceptor()
-        logging.level = HttpLoggingInterceptor.Level.BODY
-
-        instance = HttpClientBuilder.instance.newBuilder()
-                .addInterceptor { chain ->
-                    val original = chain.request()
-                    val requestBuilder = original.newBuilder()
-                            .addHeader("Cache-Control", "no-cache")
-                            .addHeader("Cache-Control", "no-store")
-                            .method(original.method(), original.body())
-                    val request = requestBuilder.build()
-
-                    return@addInterceptor chain.proceed(request)
-                }
-                .addInterceptor(logging)
-                .build()
-    }
+    val instance: OkHttpClient = HttpClientBuilder.newBuilder().build()
 }
