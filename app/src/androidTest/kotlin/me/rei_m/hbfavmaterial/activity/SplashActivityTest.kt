267b443384f352b98ade8f6a72e7adc33a8ce284
rei-m <star.oceans.rm@wj8.so-net.ne.jp>
2016-08-15 22:17:02 +0900

    update setup test

--- app/src/androidTest/kotlin/me/rei_m/hbfavmaterial/activity/SplashActivityTest.kt
+++ app/src/androidTest/kotlin/me/rei_m/hbfavmaterial/activity/SplashActivityTest.kt
@@ -53,7 +53,9 @@ class SplashActivityTest {
         val wakeUpDevice = Runnable {
             activity.window.addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON or
                     WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED or
-                    WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+                    WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON or
+                    WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD or
+                    WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON)
         }
         activity.runOnUiThread(wakeUpDevice)
     }
