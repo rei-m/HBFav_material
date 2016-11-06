881d77697379cdcfe6be6c13f45793c6697dc78f
rei-m <star.oceans.rm@wj8.so-net.ne.jp>
2016-11-07 00:14:27 +0900

    update ut of activities

--- app/src/test/kotlin/me/rei_m/hbfavmaterial/di/TestFragmentComponent.kt
+++
@@ -1,6 +0,0 @@
-//package me.rei_m.hbfavmaterial.di
-//
-//import dagger.Subcomponent
-//
-//@Subcomponent(modules = arrayOf(TestFragmentModule::class))
-//interface TestFragmentComponent : FragmentComponent
